5b24334a690f5fab77abb78b596314a1d9335ef3
yuexingxing <670176656@qq.com>
2018-03-13 15:32:51 +0800

    1
    标题修改

--- src/com/print/demo/MenuActivity.java
+++ src/com/print/demo/MenuActivity.java
@@ -23,7 +23,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
